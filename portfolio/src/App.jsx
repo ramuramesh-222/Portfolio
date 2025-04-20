62ce00ec46f3cfa07c98a28a6ec8b3f1390d1fe2
ramuramesh-222777 <ramudamy789@gmail.com>
2025-04-20 16:33:59 +0530

    start

---
+++ portfolio/src/App.jsx
@@ -0,0 +1,26 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+function App() {
+
+  return (
+    <>
+      <div className='cotainer'>
+        <header>
+          <h4>Ramesh</h4>
+          <div>
+            <ul>
+              <li>Home</li>
+              <li>About</li>
+              <li>Projects</li>
+              <li>Contact</li>
+            </ul>
+          </div>
+        </header>
+      </div>
+    </>
+  )
+}
+
+export default App
